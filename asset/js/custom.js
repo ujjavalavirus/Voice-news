7819d191188050be2c8c202e023cf758d54aa692
ujjaval <ujjaval@inkoniq.com>
2015-05-09 18:33:35 +0530

    apk

---
+++ asset/js/custom.js
@@ -0,0 +1,44 @@
+ function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+ $(document).ready(function(){
+
+ 	var lang =  getParameterByName('lang');
+ 			
+        $.ajax({ 	
+        type: 'GET', 
+        url: 'asset/js/convertcsv.json', 
+        dataType: 'json',
+        success: function (prr_dashboard, status) {
+                        //var prr = prr_dashboard.details;
+                       // alert(prr_dashboard.length);
+
+                         for(var i=0;i<prr_dashboard.length;i++){
+                         		var headline = "";
+                         		var newlang = prr_dashboard[i].language;
+                         	//	console.log(newlang);
+                         		if(newlang == lang){
+                         		var headline = "<p>"+prr_dashboard[i].headline+"</p>";
+
+                         		$("#homepagenews").append(headline);
+                         		}
+
+
+                         }
+                      
+
+
+        },
+        error: function(e){
+            console.log(e);
+        }
+});
+
+
+ });
+
+
